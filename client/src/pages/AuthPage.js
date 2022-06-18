ac5cd56aac1d58d1bc55d350c7f42cdfcea71be2
AntonOnoprienko <onoprienkoanton88@gmail.com>
2022-06-18 23:52:37 +0300

    Add hooks

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,22 +1,57 @@
 import React from "react";
 
 export const AuthPage = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const changeHandler = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
   return (
     <div className="row">
       <div className="col s6 offset-s3">
-        <h1> Сократи ссылку </h1>
+        <h2> Сократи ссылку </h2>
         <div className="card blue darken-1">
           <div className="card-content white-text">
-            <span className="card-title">Авторизация</span>
-            <p>
-              I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.
-            </p>
+            <span className="card-title">Authorization</span>
+            <div>
+              <div className="input-field">
+                <input
+                  placeholder="                 insert email"
+                  id="email"
+                  type="text"
+                  name="email"
+                  className="yellow-input"
+                  onChange={changeHandler}
+                />
+
+                <label htmlFor="email">Email:</label>
+              </div>
+              <div className="input-field">
+                <input
+                  placeholder="                  insert password"
+                  id="password"
+                  type="password"
+                  name="password"
+                  className="yellow-input"
+                  onChange={changeHandler}
+                />
+
+                <label htmlFor="password">Password:</label>
+              </div>
+            </div>
           </div>
           <div className="card-action">
-            <a href="#">This is a link</a>
-            <a href="#">This is a link</a>
+            <button
+              className="btn yellow darken-4"
+              style={{ marginRight: "10px" }}
+            >
+              Enter
+            </button>
+            <button className="btn grey lighten-1 black-text">
+              Registration
+            </button>
           </div>
         </div>
       </div>
